bad42e2808ef115814e8e468320a27b52be3beda
wdhhekop <wdhhekop@gmail.com>
2026-04-07 00:54:55 +0500

    Подключение к базе данных SQLite

---
+++ app/src/main/java/com/example/sqltask/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.sqltask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view){
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER, UNIQUE(name))");
+        db.execSQL("INSERT OR IGNORE INTO users VALUES ('Tom Smith', 23), ('John Dow', 31);");
+
+        Cursor query = db.rawQuery("SELECT * FROM users;", null);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("");
+        while(query.moveToNext()){
+            String name = query.getString(0);
+            int age = query.getInt(1);
+            textView.append("Name: " + name + " Age: " + age + "\n");
+        }
+        query.close();
+        db.close();
+    }
+}
