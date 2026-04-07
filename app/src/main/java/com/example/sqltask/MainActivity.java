5e816fcd802c4fe664e283244441f1eb941ce816
wdhhekop <wdhhekop@gmail.com>
2026-04-07 11:54:10 +0500

    Получение данных из SQLite

--- app/src/main/java/com/example/sqltask/MainActivity.java
+++ app/src/main/java/com/example/sqltask/MainActivity.java
@@ -1,34 +1,54 @@
 package com.example.sqltask;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.SimpleCursorAdapter;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView userList;
+    TextView header;
+    DatabaseHelper databaseHelper;
+    SQLiteDatabase db;
+    Cursor userCursor;
+    SimpleCursorAdapter userAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void onClick(View view){
-        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER, UNIQUE(name))");
-        db.execSQL("INSERT OR IGNORE INTO users VALUES ('Tom Smith', 23), ('John Dow', 31);");
 
-        Cursor query = db.rawQuery("SELECT * FROM users;", null);
-        TextView textView = findViewById(R.id.textView);
-        textView.setText("");
-        while(query.moveToNext()){
-            String name = query.getString(0);
-            int age = query.getInt(1);
-            textView.append("Name: " + name + " Age: " + age + "\n");
-        }
-        query.close();
+        header = findViewById(R.id.header);
+        userList = findViewById(R.id.list);
+
+        databaseHelper = new DatabaseHelper(getApplicationContext());
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        // открываем подключение
+        db = databaseHelper.getReadableDatabase();
+
+        //получаем данные из бд в виде курсора
+        userCursor =  db.rawQuery("select * from "+ DatabaseHelper.TABLE, null);
+        // определяем, какие столбцы из курсора будут выводиться в ListView
+        String[] headers = new String[] {DatabaseHelper.COLUMN_NAME, DatabaseHelper.COLUMN_YEAR};
+        // создаем адаптер, передаем в него курсор
+        userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
+                userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        header.setText("Найдено элементов: " +  userCursor.getCount());
+        userList.setAdapter(userAdapter);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        // Закрываем подключение и курсор
         db.close();
+        userCursor.close();
     }
 }
