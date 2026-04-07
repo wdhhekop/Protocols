388f697cc248e7020101e7c47ffa8a5d3f3f19a4
wdhhekop <wdhhekop@gmail.com>
2026-04-07 12:19:26 +0500

    Получение данных из SQLite

--- app/src/main/java/com/example/sqltask/MainActivity.java
+++ app/src/main/java/com/example/sqltask/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.sqltask;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.SimpleCursorAdapter;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView userList;
-    TextView header;
     DatabaseHelper databaseHelper;
     SQLiteDatabase db;
     Cursor userCursor;
@@ -22,11 +24,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        header = findViewById(R.id.header);
         userList = findViewById(R.id.list);
+        userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+                intent.putExtra("id", id);
+                startActivity(intent);
+            }
+        });
 
         databaseHelper = new DatabaseHelper(getApplicationContext());
     }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -34,18 +44,23 @@ public class MainActivity extends AppCompatActivity {
         db = databaseHelper.getReadableDatabase();
 
         //получаем данные из бд в виде курсора
-        userCursor =  db.rawQuery("select * from "+ DatabaseHelper.TABLE, null);
+        userCursor = db.rawQuery("select * from " + DatabaseHelper.TABLE, null);
         // определяем, какие столбцы из курсора будут выводиться в ListView
-        String[] headers = new String[] {DatabaseHelper.COLUMN_NAME, DatabaseHelper.COLUMN_YEAR};
+        String[] headers = new String[]{DatabaseHelper.COLUMN_NAME, DatabaseHelper.COLUMN_YEAR};
         // создаем адаптер, передаем в него курсор
         userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
                 userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
-        header.setText("Найдено элементов: " +  userCursor.getCount());
         userList.setAdapter(userAdapter);
     }
 
+    // по нажатию на кнопку запускаем UserActivity для добавления данных
+    public void add(View view) {
+        Intent intent = new Intent(this, UserActivity.class);
+        startActivity(intent);
+    }
+
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
         super.onDestroy();
         // Закрываем подключение и курсор
         db.close();
